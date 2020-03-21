596a719284524b5b0be393e9bc1fc4017c4e9df6
Rick Kleinhans <Rick Kleinhans>
2020-03-21 15:22:28 +0200

    Convert to TypeScript, and fix ESLint config including removing Prettier

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -1,24 +1,32 @@
-/**
- * SEO component that queries for data with
- *  Gatsby's useStaticQuery React hook
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
-import { useStaticQuery, graphql } from 'gatsby'
+import React, { MetaHTMLAttributes } from 'react';
+import Helmet from 'react-helmet';
+import { useStaticQuery, graphql } from 'gatsby';
 
 interface Props {
   description?: string
   lang?: string
-  meta?: []
+  meta?: {
+    name?: string | undefined
+    content: string
+    property?: string | undefined
+  }[]
   title: string
 }
 
-function SEO({ description, lang, meta, title }: Props) {
-  const { site } = useStaticQuery(
+interface SiteQuery {
+  site: {
+    siteMetadata: {
+      title: string
+      description: string
+      author: string
+    }
+  }
+}
+
+const SEO = ({
+  description = '', lang = 'en', meta = [], title,
+}: Props) => {
+  const { site }: SiteQuery = useStaticQuery(
     graphql`
       query {
         site {
@@ -29,10 +37,43 @@ function SEO({ description, lang, meta, title }: Props) {
           }
         }
       }
-    `
-  )
-
-  const metaDescription = description || site.siteMetadata.description
+    `,
+  );
+  const metaDescription = description || site.siteMetadata.description;
+  const metaItems: MetaHTMLAttributes<HTMLMetaElement>[] = [
+    {
+      name: 'description',
+      content: metaDescription,
+    },
+    {
+      property: 'og:title',
+      content: title,
+    },
+    {
+      property: 'og:description',
+      content: metaDescription,
+    },
+    {
+      property: 'og:type',
+      content: 'website',
+    },
+    {
+      name: 'twitter:card',
+      content: 'summary',
+    },
+    {
+      name: 'twitter:creator',
+      content: site.siteMetadata.author,
+    },
+    {
+      name: 'twitter:title',
+      content: title,
+    },
+    {
+      name: 'twitter:description',
+      content: metaDescription,
+    },
+  ];
 
   return (
     <Helmet
@@ -41,55 +82,9 @@ function SEO({ description, lang, meta, title }: Props) {
       }}
       title={title}
       titleTemplate={`%s | ${site.siteMetadata.title}`}
-      meta={[
-        {
-          name: `description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:title`,
-          content: title,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:creator`,
-          content: site.siteMetadata.author,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
-      ].concat(meta)}
+      meta={metaItems.concat(meta)}
     />
-  )
-}
+  );
+};
 
-SEO.defaultProps = {
-  lang: `en`,
-  meta: [],
-  description: ``,
-}
-
-SEO.propTypes = {
-  description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.arrayOf(PropTypes.object),
-  title: PropTypes.string.isRequired,
-}
-
-export default SEO
+export default SEO;
