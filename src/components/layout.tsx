f54a1c62a630aa782696c5bc54f58fe5720f8af3
Rick Kleinhans <Rick Kleinhans>
2020-03-24 20:01:15 +0200

    Edit photo, add icon, add social links, style header

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
 import Header from './header';
 import './layout.css';
 
@@ -7,39 +6,32 @@ interface Props {
   children: React.ReactNode
 }
 
-const Layout = ({ children }: Props) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `);
-
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
+const Layout = ({ children }: Props) => (
+  <>
+    <Header />
+    <div
+      style={{
+        margin: '0 auto',
+        maxWidth: 960,
+        padding: '0 1.0875rem 1.45rem',
+      }}
+    >
+      <main>{children}</main>
+      <footer
         style={{
-          margin: '0 auto',
-          maxWidth: 960,
-          padding: '0 1.0875rem 1.45rem',
+          textAlign: 'center',
         }}
       >
-        <main>{children}</main>
-        <footer>
-          ©
-          {' '}
-          {new Date().getFullYear()}
-          , Built with
-          {' '}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  );
-};
+        Made with
+        {' '}
+        <span role="img" aria-labelledby="love">❤️</span>
+        {' '}
+        by
+        {' '}
+        <a href="https://twitter.com/rickkln">@rickkln</a>
+      </footer>
+    </div>
+  </>
+);
 
 export default Layout;
--- src/pages/cv.tsx
+++ src/pages/cv.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import Layout from '../components/layout';
-import Image from '../components/photo';
 import SEO from '../components/seo';
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
-    </div>
+    <SEO title="CV" />
+    <h1>CV</h1>
+    <p>Welcome to cv.</p>
   </Layout>
 );
 
