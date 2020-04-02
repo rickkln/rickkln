7f2292ea94a03c6052a0baeac51a5c48b18a43cb
Rick Kleinhans <Rick Kleinhans>
2020-04-03 00:12:01 +0200

    Add initial page content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,30 @@ import SEO from '../components/seo';
 const IndexPage = () => (
   <Layout>
     <SEO title="About" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>
+      My name is Rick, and I make things. I enjoy making apps, websites,
+      companies, beer, wine, music, adventures happen, and my wife happy.
+    </p>
+    <p>
+      I am currently working as an engineering manager at
+      {' '}
+      <a href="https://luno.com">Luno</a>
+      . There I help a couple of our teams deliver some of the most loved, and widely used,
+      products in the crypto industry.
+    </p>
+    <p>
+      I have spent all of my career as either a founder or an early employee at various startups,
+      helping companies make products their customers love. Immediately preceding my time at Luno
+      I helped
+      {' '}
+      <a href="https://activitar.com">Activitar</a>
+      {' '}
+      on their journey from being idea to becoming the largest activity platform in Africa.
+    </p>
+    <p>
+      Throughout my career I have tinkered primarily in JavaScript and Swift, but have also dabbled
+      in Ruby, Python, Kotlin, Java, and C.
+    </p>
   </Layout>
 );
 
